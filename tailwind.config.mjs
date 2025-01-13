e8525f7673f554eca9d14fddffe401f116683456
Sachin Sharma <155435532+iamSachin08@users.noreply.github.com>
2025-01-13 14:52:11 +0530

    first commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import { Outfit } from 'next/font/google';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -5,13 +7,30 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  safelist: [
+    'bg-[url("/footer-bg-color.png")]',
+  ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        lightHover: '#fcf4ff',
+        darkHover:  '#2a004a',
+        darkTheme:  '#11001F'
       },
+      fontFamily:{
+        Outfit:["Outfit","sans-serif"],
+        Ovo:   ["Ovo","serif"]
+      },
+      boxShadow:{
+        'black':'4px 4px 0 #000',
+        'white': '4px 4px 0 #fff'
+      },
+      gridTemplateColumns:{
+        'auto':'repeat(auto-fit,minmax(200px,1fr))'
+      },
+     
     },
   },
+  darkMode:'selector',
   plugins: [],
 };
